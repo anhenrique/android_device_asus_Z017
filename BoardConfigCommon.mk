7f59820b0f43082a9f3d5d8a6938444b54f42731
Anand Ramachandran <anand.nv1998@gmail.com>
2017-11-06 12:33:28 +0530

    Bringup Common for Asus zenfone3
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2017 AIMROM
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+TARGET_BOOTLOADER_BOARD_NAME := MSM8953
+
+include device/asus/*/BoardConfigCommon.mk
+
+-include vendor/asus/Z017/BoardConfigVendor.mk
+
+PLATFORM_PATH := device/asus/msm8953-common
+
+# Properties
+TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
---
+++ README.md
@@ -0,0 +1,4 @@
+Unified Device Tree for Asus zenfone3 MSM8953 Family
+===============================================
+
+Copyright 2017 - AIMROM
