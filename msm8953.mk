07df15fbe34e52e2b13c27481b03bdaf6aa75f98
Anand Ramachandran <anand.nv1998@gmail.com>
2017-11-07 18:51:12 +0530

    Build Bluetooth hal

--- msm8953.mk
+++ msm8953.mk
@@ -211,6 +211,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Bluetooth
+ PRODUCT_PACKAGES += \
+	 libbt-vendor \
+     android.hardware.bluetooth@1.0-impl
+ 
+
 # Browser
 PRODUCT_PACKAGES += \
     Gello
