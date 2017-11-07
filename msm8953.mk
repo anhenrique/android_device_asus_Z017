a411544bbcd636bb20dc55ede8c7a8fed3cf1934
Anand Ramachandran <anand.nv1998@gmail.com>
2017-11-07 17:48:27 +0530

    Update audio for O
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -62,18 +62,23 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audiod \
-    audio.a2dp.default \
     audio.primary.msm8953 \
-    audio.r_submix.default \
+    audio.a2dp.default \
     audio.usb.default \
-    libbthost_if \
+    audio.r_submix.default \
     libaudio-resampler \
+
+PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcompostprocbundle \
-    libvolumelistener \
-    tinymix
+    libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/configs/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
